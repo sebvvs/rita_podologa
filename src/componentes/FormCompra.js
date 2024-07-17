9a5be15a7d8500c7398c0300ccbb7894e979850e
sebvvs <sebaas.ml64@gmail.com>
2024-07-17 01:04:41 -0400

    actualizacion form

--- src/componentes/FormCompra.js
+++ src/componentes/FormCompra.js
@@ -1,30 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const regionesYComunas = {
+    "Región de Arica y Parinacota": ["Arica", "Camarones", "Putre", "General Lagos"],
+    "Región de Tarapacá": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"],
+    "Región de Antofagasta": ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "Tocopilla", "María Elena"],
+    "Región de Atacama": ["Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Huasco", "Freirina", "Alto del Carmen"],
+    "Región de Coquimbo": ["La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paiguano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"],
+    "Región de Valparaíso": ["Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "Santo Domingo", "San Felipe", "Catemu", "Llaillay", "Panquehue", "Putaendo", "Santa María", "Quilpué", "Limache", "Olmué", "Villa Alemana"],
+    "Región del Libertador General Bernardo O'Higgins": ["Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"],
+    "Región del Maule": ["Talca", "Constitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"],
+    "Región de Ñuble": ["Chillán", "Bulnes", "Cobquecura", "Coelemu", "Coihueco", "El Carmen", "Ninhue", "Ñiquén", "Pemuco", "Pinto", "Portezuelo", "Quillón", "Quirihue", "Ránquil", "San Carlos", "San Fabián", "San Ignacio", "San Nicolás", "Treguaco", "Yungay"],
+    "Región del Biobío": ["Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío"],
+    "Región de La Araucanía": ["Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre Las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol", "Angol", "Collipulli", "Curacautín", "Ercilla", "Lonquimay", "Los Sauces", "Lumaco", "Purén", "Renaico", "Traiguén", "Victoria"],
+    "Región de Los Ríos": ["Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"],
+    "Región de Los Lagos": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "Frutillar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"],
+    "Región de Aysén del General Carlos Ibáñez del Campo": ["Coyhaique", "Lago Verde", "Aysén", "Cisnes", "Guaitecas", "Cochrane", "O'Higgins", "Tortel", "Chile Chico", "Río Ibáñez"],
+    "Región de Magallanes y de la Antártica Chilena": ["Punta Arenas", "Laguna Blanca", "Río Verde", "San Gregorio", "Cabo de Hornos", "Antártica", "Porvenir", "Primavera", "Timaukel", "Natales", "Torres del Paine"],
+    "Región Metropolitana de Santiago": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltil", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"],
+};
 
 function FormularioCompra() {
+    const [comunas, setComunas] = useState([]);
+    const [selectedRegion, setSelectedRegion] = useState("");
+    const [fecha, setFecha] = useState("");
+
+    useEffect(() => {
+        const fechaActual = new Date().toISOString().split('T')[0];
+        setFecha(fechaActual);
+    }, []);
+
+    const handleRegionChange = (event) => {
+        const region = event.target.value;
+        setSelectedRegion(region);
+        setComunas(regionesYComunas[region] || []);
+    };
+
     return (
-        <section class="seccion" id="formulario-compra" style={{ display: 'none' }}>
-        <h2>Información de Compra</h2>
-        <form id="form-compra">
-            <div class="campo">
-                <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" required />
-            </div>
-            <div class="campo">
-                <label for="rut">RUT:</label>
-                <input type="text" id="rut" name="rut" required />
-            </div>
-            <div class="campo">
-                <label for="direccion">Dirección:</label>
-                <input type="text" id="direccion" name="direccion" required /> 
-            </div>
-            <div class="campo">
-                <label for="comuna">Comuna:</label>
-                <input type="text" id="comuna" name="comuna" required />
-            </div>
-            <button type="submit">Confirmar Compra</button>
-        </form>
-    </section>
-    )
+        <section className="seccion" id="formulario-compra" style={{ display: 'none' }}>
+            <h2>Información de Compra</h2>
+            <form id="form-compra">
+                <div className="campo">
+                    <label htmlFor="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" required />
+                </div>
+                <div className="campo">
+                    <label htmlFor="rut">RUT:</label>
+                    <input type="text" id="rut" name="rut" required />
+                </div>
+                <div className="campo">
+                    <label htmlFor="direccion">Dirección:</label>
+                    <input type="text" id="direccion" name="direccion" required />
+                </div>
+                <div className="campo">
+                    <label htmlFor="region">Región:</label>
+                    <select id="region" name="region" value={selectedRegion} onChange={handleRegionChange} required>
+                        <option value="" disabled>Selecciona una región</option>
+                        {Object.keys(regionesYComunas).map(region => (
+                            <option key={region} value={region}>{region}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="campo">
+                    <label htmlFor="comuna">Comuna:</label>
+                    <select id="comuna" name="comuna" required>
+                        <option value="" disabled>Selecciona una comuna</option>
+                        {comunas.map(comuna => (
+                            <option key={comuna} value={comuna}>{comuna}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="campo">
+                    <label htmlFor="fecha">Fecha:</label>
+                    <input type="text" id="fecha" name="fecha" value={fecha} readOnly />
+                </div>
+                <div className="campo">
+                    <label htmlFor="terminos">Acepto los términos y condiciones</label>
+                    <input type="checkbox" id="terminos" name="termino" required />
+                </div>
+                <button type="submit">Confirmar Compra</button>
+            </form>
+        </section>
+    );
 }
 
 export default FormularioCompra;
--- src/componentes/GestionDatos.js
+++ src/componentes/GestionDatos.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function GestionDatos() {
     return (
-        <section class="seccion" id="gestion-datos" style={{ display: 'none' }}>
-        <h2>Datos de Compra Guardados</h2>
-        <div id="datos-compra">
-            {/* Los datos de compra se mostrarán aquí dinámicamente */}
-        </div>
-    </section>
-    )
+        <section className="seccion" id="gestion-datos" style={{ display: 'none' }}>
+            <h2>Datos de Compra Guardados</h2>
+            <div id="datos-compra">
+                {/* Los datos de compra se mostrarán aquí dinámicamente */}
+            </div>
+        </section>
+    );
 }
 
 export default GestionDatos;
